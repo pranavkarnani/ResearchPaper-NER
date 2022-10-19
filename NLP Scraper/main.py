6b838a68ffb8b2a14d15d25f4f0eb7dc0eff6653
pranavkarnani <pranavkarnani@pranavs-mbp-2.wifi.local.cmu.edu>
2022-10-19 17:32:15 -0400

    Rebase

---
+++ NLP Scraper/main.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+from utils.strings import *
+from scraper.master import crawl_url
+from twisted.internet import reactor
+
+def setup():
+    try:
+        if not os.path.exists("./papers"):
+            os.makedirs("./papers")
+
+    except:
+        pass
+
+    file = open("./papers/papers.tsv", "w")
+    file.close()
+
+    file = open("./papers/errors.tsv", "w")
+    file.close()
+
+    with open("./papers/papers.tsv", 'a', encoding='utf8', newline='') as papers:
+        tsv_writer = csv.writer(papers, delimiter='\t', lineterminator='\n')
+        tsv_writer.writerow([paperKeys['name'], paperKeys['url'], paperKeys['download'], paperKeys['abstract']])
+
+
+setup()
+crawl_url()
+reactor.run()
